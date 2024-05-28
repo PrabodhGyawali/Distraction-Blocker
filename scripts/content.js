ed6df8e778c5a6112a263787a28af34f8c2f861c
Prabodhcoolkid <prabodh.gyawali2005@gmail.com>
2024-05-29 00:43:23 +0100

    Cleared Youtube

--- scripts/content.js
+++ scripts/content.js
@@ -1,36 +1,41 @@
 // Hides the youtube recommendations
 window.onload = function() {
-    console.log("script loaded");
-    thumbnails = document.querySelector('#contents.ytd-rich-grid-renderer')
-
+    // Remove Thumbnails
+    thumbnails = document.querySelector('#contents.ytd-rich-grid-renderer');
     if (thumbnails) {
-        thumbnails.style.visibility = "hidden";
-        console.log("Youtube recommendations hidden");
+        thumbnails.style.display = "none";
+    }
+    // Remove Page Manager
+    pageManager = document.querySelector('ytd-page-manager');
+    if (pageManager) {
+        pageManager.style.display = "none";
+    }
+    // Remove Left-Side Bar Sections: 5 Sections
+    // More from Youtube
+
+    // Reacting to a change in the DOM
+    const observer = new MutationObserver((mutationList, observer) => {
+        mutationList.forEach(mutation => {
+            // Customize the Sections -> Watch-later is only allowed
+            if (mutation.target.id === 'sections') {
+                sections = document.querySelector('#sections');
+                if (sections) {
+                    sections.style.display = "none";
+                }
+            }
+            // Remove Footer
+            if (mutation.target.id === 'footer') {
+                secondary = document.querySelector('#footer');
+                if (secondary) {
+                    secondary.style.display = "none";
+                }
+            }   
+        })
+    });
+    observer.observe(document, { childList: true, subtree: true });
+    
+    footer = document.querySelector('#footer');
+    if (footer) {
+        footer.style.display = "none";
     }
 };
-
-// Example Code from Youtube Extension
-// const homepage = document.querySelector('ytd-rich-grid-renderer');
-
-// function removeContent() {
-//     const homepage = document.querySelector('ytd-rich-grid-renderer');
-//     if (homepage) {
-//         Array.prototype.forEach.call(homepage.children, (child) => {
-//             child.style.display = 'none';
-//         });
-//     }
-// }
-
-// var observer = new MutationObserver((mutations) => {
-//     mutations.forEach((mutation) => {
-//         if (mutation.addedNodes.length) {
-//             removeContent();
-//         }
-//     });
-// });
-
-// removeContent();
-
-// window.scrollTo(0, 0);
-// const yt = document.querySelector('#contents');
-// observer.observe(yt, { childList: true });
