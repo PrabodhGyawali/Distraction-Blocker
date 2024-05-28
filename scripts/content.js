eeaff4b34c877c19ef2979c2ed8b5ad9cd48f159
Prabodhcoolkid <prabodh.gyawali2005@gmail.com>
2024-05-28 23:35:58 +0100

    Thumbnail-hidden

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,36 @@
 // Hides the youtube recommendations
-alert("script loaded");
-thumbnails = document.querySelector('#contents.ytd-rich-grid-renderer')
+window.onload = function() {
+    console.log("script loaded");
+    thumbnails = document.querySelector('#contents.ytd-rich-grid-renderer')
 
-if (thumbnails) {
-    thumbnails.style.visibility = "hidden";
-    console.log("Youtube recommendations hidden");
-}
+    if (thumbnails) {
+        thumbnails.style.visibility = "hidden";
+        console.log("Youtube recommendations hidden");
+    }
+};
+
+// Example Code from Youtube Extension
+// const homepage = document.querySelector('ytd-rich-grid-renderer');
+
+// function removeContent() {
+//     const homepage = document.querySelector('ytd-rich-grid-renderer');
+//     if (homepage) {
+//         Array.prototype.forEach.call(homepage.children, (child) => {
+//             child.style.display = 'none';
+//         });
+//     }
+// }
+
+// var observer = new MutationObserver((mutations) => {
+//     mutations.forEach((mutation) => {
+//         if (mutation.addedNodes.length) {
+//             removeContent();
+//         }
+//     });
+// });
+
+// removeContent();
+
+// window.scrollTo(0, 0);
+// const yt = document.querySelector('#contents');
+// observer.observe(yt, { childList: true });
