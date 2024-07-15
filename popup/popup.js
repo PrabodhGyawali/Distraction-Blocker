6488d15136a95294a51cb3d2663063bbf60c5db2
Prabodhcoolkid <prabodh.gyawali2005@gmail.com>
2024-07-15 16:00:22 +0100

    Revert "chrome storage"
    This reverts commit deaa1cf76dd941ebd5792815fc5e1ded7b38c8ee.

--- popup/popup.js
+++ popup/popup.js
@@ -1,11 +1,1 @@
-// Youtube Settings:
-
-function ytSettings() {
-    document.getElementById("yt-settings").style.display = "block";
-    // read word list from storage
-    chrome.storage.sync.get('ytWordList', function(data) {
-        if (data.ytWordList) {
-            document.getElementById("yt-word-list").value = data.ytWordList;
-        }
-    });
-}
+console.log("This is a popup!");
