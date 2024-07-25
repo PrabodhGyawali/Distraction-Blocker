0e510dace236d3e3f98f5742c2ab0efc5dac5e79
Prabodhcoolkid <prabodh.gyawali2005@gmail.com>
2024-07-25 10:29:34 +0100

    Working on Popup settings

--- scripts/instagram-content.js
+++ scripts/instagram-content.js
@@ -1,5 +1,20 @@
-var currentUrl = window.location.href;
-
-if (!currentUrl.includes('/direct/')) {
-    window.location.href = 'https://www.instagram.com/direct/inbox/';
+function noise_elimination() {
+    var currentURL = window.location.href;
+    console.log('Current URL:', currentURL);
+    if (!currentURL.includes('/direct/')) {
+        window.location.href = 'https://www.instagram.com/direct/inbox/';
+    }
 }
+
+window.onload = noise_elimination();
+
+// Prevent dynamic behavior loop of the page
+window.addEventListener('click', (e) => {
+    if (e.target.href) {
+        if (e.target.href.includes('instagram.com') && !e.target.href.includes('/direct/')) {
+            window.location.href = 'https://www.instagram.com/direct/inbox/';
+        }
+    };
+    noise_elimination();
+});
+
